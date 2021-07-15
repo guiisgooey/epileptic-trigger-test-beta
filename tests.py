019f3c3bc17a50871be754f97cbce38b2271b053
guiisgooey <49354894+guiisgooey@users.noreply.github.com>
2021-07-15 03:18:29 -0400

    removed main.py, added to requirements, added assertions to tests

--- tests.py
+++ tests.py
@@ -1,19 +1,19 @@
 from gif_test import frame_brightness_test
 
 #---Test that flashing gifs are detected as flashing, non-flashing gifs are not---
-frame_brightness_test('gifs/flashing2.gif')
+assert frame_brightness_test('gifs/flashing2.gif') == True
 
 print('----------break----------')
 
-frame_brightness_test('gifs/flashing.gif')
+assert frame_brightness_test('gifs/flashing.gif') == True
 
 print('----------break----------')
 
-frame_brightness_test('gifs/animation.gif')
+assert frame_brightness_test('gifs/animation.gif') == False
 
 print('----------break----------')
 
-frame_brightness_test('gifs/this nuts.gif')
+assert frame_brightness_test('gifs/this nuts.gif') == True
 
 
 
